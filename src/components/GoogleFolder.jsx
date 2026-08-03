2e9eb027425c7c567632dfe0c1f271582e351ed5
Winston Gultiano <wgultiano08@gmail.com>
2026-08-03 08:52:38 +0800

    Day 4: Done Messenger Icon.

--- src/components/GoogleFolder.jsx
+++ src/components/GoogleFolder.jsx
@@ -11,78 +11,78 @@ import GoogleAssistantIcon from '../icons/google-assistant-icon.png'
 function GoogleFolder() {
     return (
         <>
-        <div className="google-folder">
+            <div className="google-folder">
 
-            <a href="https://www.google.com" target="_blank" rel="noreferrer">
-                <img
-                    className="app-icon google-icon"
-                    src={GoogleIcon}
-                    alt="Google"
-                />
-            </a>
+                <a href="https://www.google.com" target="_blank" rel="noreferrer">
+                    <img
+                        className="app-icon google-icon"
+                        src={GoogleIcon}
+                        alt="Google"
+                    />
+                </a>
 
-            <a href="https://mail.google.com" target="_blank" rel="noreferrer">
-                <img
-                    className="app-icon gmail-icon"
-                    src={GmailIcon}
-                    alt="Gmail"
-                />
-            </a>
+                <a href="https://mail.google.com" target="_blank" rel="noreferrer">
+                    <img
+                        className="app-icon gmail-icon"
+                        src={GmailIcon}
+                        alt="Gmail"
+                    />
+                </a>
 
-            <a href="https://maps.google.com" target="_blank" rel="noreferrer">
-                <img
-                    className="app-icon map-icon"
-                    src={MapIcon}
-                    alt="Map"
-                />
-            </a>
+                <a href="https://maps.google.com" target="_blank" rel="noreferrer">
+                    <img
+                        className="app-icon map-icon"
+                        src={MapIcon}
+                        alt="Map"
+                    />
+                </a>
 
-            <a href="https://drive.google.com" target="_blank" rel="noreferrer">
-                <img
-                    className="app-icon gdrive-icon"
-                    src={GdriveIcon}
-                    alt="Gdrive"
-                />
-            </a>
+                <a href="https://drive.google.com" target="_blank" rel="noreferrer">
+                    <img
+                        className="app-icon gdrive-icon"
+                        src={GdriveIcon}
+                        alt="Gdrive"
+                    />
+                </a>
 
-            <a href="https://music.youtube.com" target="_blank" rel="noreferrer">
-                <img className="app-icon yt-music-icon"
-                     src={YTMusicIcon} 
-                     alt="YT Music" 
-                />
-            </a>
+                <a href="https://music.youtube.com" target="_blank" rel="noreferrer">
+                    <img className="app-icon yt-music-icon"
+                        src={YTMusicIcon}
+                        alt="YT Music"
+                    />
+                </a>
 
-            <a href="https://tv.google.com" target="_blank" rel="noreferrer">
-                <img className="app-icon google-tv-icon"
-                src={GoogleTVIcon} 
-                alt="Google TV" 
-                />
-            </a>
+                <a href="https://tv.google.com" target="_blank" rel="noreferrer">
+                    <img className="app-icon google-tv-icon"
+                        src={GoogleTVIcon}
+                        alt="Google TV"
+                    />
+                </a>
 
-            <a href="https://meet.google.com" target="_blank" rel="noreferrer">
-                <img className="app-icon google-meet-icon"
-                     src={GoogleMeetIcon}
-                     alt="Google Meet"
-                />
-            </a>
+                <a href="https://meet.google.com" target="_blank" rel="noreferrer">
+                    <img className="app-icon google-meet-icon"
+                        src={GoogleMeetIcon}
+                        alt="Google Meet"
+                    />
+                </a>
 
-            <a href="https://photos.google.com" target="_blank" rel="noreferrer">
-                <img className="app-icon google-photos-icon"
-                     src={GooglePhotosIcon}
-                     alt="Google Photos"
-                />
-            </a>
+                <a href="https://photos.google.com" target="_blank" rel="noreferrer">
+                    <img className="app-icon google-photos-icon"
+                        src={GooglePhotosIcon}
+                        alt="Google Photos"
+                    />
+                </a>
 
-            <a href="https://assistant.google.com" target="_blank" rel="noreferrer">
-                <img className="app-icon google-assistant-icon"
-                     src={GoogleAssistantIcon}
-                     alt="Google Assistant"
-                />
-            </a>
+                <a href="https://assistant.google.com" target="_blank" rel="noreferrer">
+                    <img className="app-icon google-assistant-icon"
+                        src={GoogleAssistantIcon}
+                        alt="Google Assistant"
+                    />
+                </a>
 
-        </div>
+            </div>
 
-        <p className="google-folder-label">Google</p>
+            <p className="google-folder-label">Google</p>
         </>
     )
 }
--- src/components/SocialFolder.jsx
+++ src/components/SocialFolder.jsx
@@ -1,8 +1,27 @@
+import MessengerIcon from '../icons/messenger-icon.png'
+
+
+
 function SocialFolder() {
 
     return (
         <>
         <div className="social-folder">
+
+            <a href="https://www.messenger.com" target="_blank" rel="noreferrer" >
+                <img
+                    className="app-icon messenger-icon"
+                    src={MessengerIcon}
+                    alt="Messenger" 
+                />
+            </a>
+
+            {/* <a href="https://www.youtube.com" target="_blank" rel="noreferrer">
+                <img 
+                src={YouTubeIcon} 
+                alt=""
+                />
+            </a> */}
 
         </div>
 
