0146705e23ce7e1d030a6dc28ebcd7c6019e6ead
Winston Gultiano <wgultiano08@gmail.com>
2026-08-14 20:27:27 +0800

    Adding tools folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Homescreen from "./tracks/Homescreen"
 import GoogleFolder from "./components/GoogleFolder"
 import SocialFolder from "./components/SocialFolder"
+import ToolsFolder from "./components/ToolsFolder"
+
 
 function App() {
 
@@ -9,6 +11,7 @@ function App() {
     <Homescreen />
     <GoogleFolder />
     <SocialFolder />
+    <ToolsFolder />
     </>
   )
 }
---
+++ src/components/ToolsFolder.jsx
@@ -0,0 +1,11 @@
+function ToolsFolder() {
+    return (
+        <>
+            <div className="tools-folder">
+
+            </div>
+        </>
+    )
+}
+
+export default ToolsFolder
