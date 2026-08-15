62769da80461d8691667ee94bd8e9b80cfd22165
Winston Gultiano <wgultiano08@gmail.com>
2026-08-15 21:53:01 +0800

    Adding Calculator Icon to Tools Folder

--- src/components/ToolsFolder.jsx
+++ src/components/ToolsFolder.jsx
@@ -1,8 +1,19 @@
+import Calculator from '../icons/calculator-icon.png'
+
+
+
 function ToolsFolder() {
     return (
         <>
             <div className="tools-folder">
 
+                <a href="https://calculator.apps.chrome/" target="_blank" rel="noreferrer">
+                    <img 
+                        className="app-icon tools-folder-icon" 
+                        src={Calculator}
+                        alt="Calculator"
+                    />
+                </a>    
             </div>
         </>
     )
