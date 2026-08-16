234f3baa63f8c428c059e5f7ef4ad83e68418c8f
Winston Gultiano <wgultiano08@gmail.com>
2026-08-16 12:08:13 +0800

    Adding Music Icon to Daily Used Folder

--- src/components/DailyUsedFolder.jsx
+++ src/components/DailyUsedFolder.jsx
@@ -1,4 +1,6 @@
 import FilesIcon from '../icons/files-icon.png'
+import MusicIcon from '../icons/music-icon.png'
+// import ArcPlayerIcon from '../icons/arc-player-icon.png'
 
 
 function DailyUsedFolder() {
@@ -13,6 +15,22 @@ function DailyUsedFolder() {
                         alt="Files"
                     />
                 </a>
+                
+                <a href="">
+                    <img
+                        className="app-icon daily-used-folder-icon"
+                        src={MusicIcon}
+                        alt="Music"
+                    />
+                </a>
+
+                {/* <a href="">
+                    <img
+                        className="app-icon daily-used-folder-icon"
+                        src={ArcPlayerIcon}
+                        alt="Arc Player"
+                    />
+                </a> */}
 
             </div>
             <p className="daily-used-folder-label">Daily Used</p>
