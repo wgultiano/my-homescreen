bd7296710a739f978c64247de6eb2cfbbdad2686
Winston Gultiano <wgultiano08@gmail.com>
2026-08-22 16:30:20 +0800

    Adding Meteored to Left Home Screen

--- src/screen/LeftHomeScreen.jsx
+++ src/screen/LeftHomeScreen.jsx
@@ -1,14 +1,32 @@
 import Wallpaper from '../assets/images/wallpaper.jpg'
+import MeteoredIcon from '../assets/icons/meteored-icon.png'
 import AppDock from '../components/AppDock/AppDock'
 import './LeftHomeScreen.css'
 
 
 function LeftHomeScreen () {
     return (
+        <>
         <div className="left-homescreen">
             <img className="wallpaper" src={Wallpaper} alt="Wallpaper" />
             <AppDock />
         </div>
+
+        <div className="left-app-grid">
+            <a href="https://www.meteored.com/" target="_blank" rel="noreferrer">
+                <img
+                    className="app-icon meteored-icon"
+                    src={MeteoredIcon}
+                    alt="Meteored"
+                />
+                <p className="meteored-label">Meteored</p>
+            </a>
+
+
+
+        </div>
+
+        </>
     )
 
 }
