b3420d42603daceb6319b1bf145f410c84998435
Winston Gultiano <wgultiano08@gmail.com>
2026-08-22 21:02:07 +0800

    Adding Palm Store to Left Home Screen

--- src/screen/LeftHomeScreen.jsx
+++ src/screen/LeftHomeScreen.jsx
@@ -2,6 +2,7 @@ import Wallpaper from '../assets/images/wallpaper.jpg'
 import MeteoredIcon from '../assets/icons/meteored-icon.png'
 import AIGalleryIcon from '../assets/icons/ai-gallery-icon.png'
 import KeepNotesIcon from '../assets/icons/keep-notes-icon.png'
+import PalmStoreIcon from '../assets/icons/palm-store-icon.png'
 import AppDock from '../components/AppDock/AppDock'
 import './LeftHomeScreen.css'
 
@@ -40,6 +41,15 @@ function LeftHomeScreen () {
                     alt="Keep Notes"
                 />
                 <p className="keep-notes-label">Keep Notes</p>
+            </a>
+
+            <a href="https://www.palmplaystore.com/" target="_blank" rel="noreferrer">
+                <img
+                    className="app-icon palm-store-icon"
+                    src={PalmStoreIcon}
+                    alt="Palm Store"
+                />
+                <p className="palm-store-label">Palm Store</p>
             </a>
 
         </div>
